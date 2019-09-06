077b2b1680eef8654d4bb0feb483b3e72b1f11ab
danielma911 <dma@paloaltonetworks.com>
2019-09-07 03:36:14 +0800

    change_back

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http Daniel's demo aaaaaaaa  example"
+	title := "Jenkins X golang http Daniel's demo  example"
 
 	from := ""
 	if r.URL != nil {
